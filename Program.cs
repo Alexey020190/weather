2c394d8387891ba9de72af1906e5f871cd75af7c
Alex <57088717+Alexey020190@users.noreply.github.com>
2021-07-15 21:07:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace TestWork
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string url = "https://goweather.herokuapp.com/weather/Kyiv";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string response;
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                response = streamReader.ReadToEnd();
+
+            Console.WriteLine(response);
+        } 
+    }
+}
